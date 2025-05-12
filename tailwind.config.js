eb14395222d371e1bd5a237fca42c4021af43b4a
Aaron Schiemann <schiemann_aaron@web.de>
2025-05-12 20:43:58 +0200

    Style-Update: Animierte Rahmen, kontrastreiche Farbverläufe und grauer Innenbereich

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      animation: {
+        'gradient-border': 'gradient-rotate 4s linear infinite'
+      },
+      keyframes: {
+        'gradient-rotate': {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' }
+        }
+      }
+    }
   },
   plugins: [],
 }
